c9572d0fd354addf2cdac7b9531fc38842164eca
Amatsukaze <lls0308abc@gmail.com>
2017-12-21 21:18:50 +0800

    Product Module: Add Product class and access methods

---
+++ src/edu/ntou/cse/softwareengineering/ees/Product.java
@@ -0,0 +1,89 @@
+package edu.ntou.cse.softwareengineering.ees;
+
+import java.lang.String;
+import java.util.*;
+
+public class Product {
+	
+	private final int INVALID = -1;
+	private int productId = INVALID;
+	private int holderId = INVALID;
+	private String productName;
+	private String description;
+	private int price = INVALID;
+	private ArrayList<String> tags;
+	
+	public Product(int productId, int holderId, String productName, String description,
+			int price, ArrayList<String> tags) 
+	{
+		if(productId != INVALID && holderId != INVALID)
+		{
+			this.productId = productId;
+			this.holderId = holderId;
+			this.productName = productName;
+			this.description = description;
+			this.price = price;
+			this.tags = tags;
+		}
+	}
+	
+	public int getProductId() {
+		return productId;
+	}
+	
+	public int getHolderId() {
+		return holderId;
+	}
+	
+	public String getProductName() {
+		return productName;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public ArrayList<String> getTags(){
+		return tags;
+	}
+	
+	public boolean setProductName(String productName) {
+		if(productName != null && productName != "")
+		{
+			this.productName = productName;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean setDescription(String description) {
+		if(description != null && description != "")
+		{
+			this.description = description;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean setPrice(int price) {
+		if(price >= 0)
+		{
+			this.price = price;
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean setTags(ArrayList<String> tags) {
+		if(tags != null)
+		{
+			this.tags = tags;
+			return true;
+		}
+		return false;
+	}
+}
